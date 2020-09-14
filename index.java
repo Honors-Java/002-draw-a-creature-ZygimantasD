f348f4f583050a13bccf2929937665736f721730
Zygimantas Daunoras <daunoraszygimantas@gmail.com>
2020-09-14 18:01:42 +0000

    Created my Meeseeks monster!

--- index.java
+++ index.java
@@ -11,7 +11,47 @@ void draw() {
  
   //Make a creature!!! 
   //all of your programming should be after this 👍
+
+  //hair
+  stroke(0);
+  fill(255, 145, 0);
+  rect(200, 100, 20, 30);
+
+  //body
+  stroke(0);
+  fill(83, 206, 228);
+  rect(200, 325, 50, 100);
+
+  //head
+  stroke(0);
+  fill(83, 206, 228);
+  ellipse(200, 200, 200, 200);
+
+  //eyes
+  fill(0);
+  ellipse(185, 200, 20, 20);
+
+  fill(0);
+  ellipse(215, 200, 20, 20);
+
+  //pupils
+  fill(255);
+  ellipse(184, 201, 10, 10);
+
+  fill(255);
+  ellipse(215, 200, 10, 10);
   
+  //mouth
+  stroke(0);
+  fill(99, 33, 33);
+  ellipse(200, 255, 100, 50);
+
+  //eye brows
+  stroke(255, 145, 0);
+  line(190, 180, 167, 182);
+
+  stroke(255, 145, 0);
+  line(210, 180, 233, 182);
  
   
   
